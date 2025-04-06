0e48f43a331a8ce1cdb0cc846872a817f60ae7bb
Gabriel <caceresgabriel305@gmail.com>
2025-04-06 18:51:49 -0500

    Agregar configuración de Nixpacks y actualizar la configuración de Vite

--- vite.config.js
+++ vite.config.js
@@ -2,20 +2,19 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    base: 'https://ayrton-laravel-production.up.railway.app/build/',
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: ['resources/views/**/*.blade.php'],
         }),
     ],
-    optimizeDeps: {
-        include: ['esbuild'],
-    },
     server: {
         host: '0.0.0.0',
-        port: 5173,      
+        port: 5173,
         hmr: {
-            host: '', 
-        }
-    }
+            host: 'ayrton-laravel-production.up.railway.app',
+            protocol: 'wss',
+        },
+    },
 });
